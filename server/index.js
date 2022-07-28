f7f29d02ffab568fdf490e6d60476438b49401ac
dangchien <82793130+vdchien98@users.noreply.github.com>
2022-07-28 15:13:57 +0700

    lam back end

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const db = mysql.createConnection({
     port: 3307,
 });
 
+// Tạo thông tin
 app.post('/create', (req, res) => {
     const name = req.body.name;
     const age = req.body.age;
@@ -38,6 +39,28 @@ app.get('/employees', (req, res) => {
         }
     });
 });
+
+//  Sửa thông tin :
+app.put('/update', (req, res) => {
+    const id = req.body.id;
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+    db.query(
+        'UPDATE employees SET name=? , age = ?, country = ?, position = ?,wage = ? WHERE id = ?',
+        [name, age, country, position, wage, id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
 app.listen(3001, () => {
     console.log('Hello your server is running on port 3001');
 });
